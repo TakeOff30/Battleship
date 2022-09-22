3fbc6c014b57bef86c4e4d4ae433e9693dbdab5d
Oliver <oliver20669@gmail.com>
2022-09-22 11:27:06 +0200

    Implementing UI logic

--- src/Game.js
+++ src/Game.js
@@ -4,42 +4,55 @@ import Player from './scripts/Player';
 import Gameboard from './scripts/Gameboard';
 
 export default class Game {
-	constructor() {
-		this.player = null;
-		this.cpu = null;
-		this.dir = null;
-	}
+    constructor() {
+        this.player = null;
+        this.cpu = null;
+        this.dir = null;
+    }
 
-	static main() {
-		this.setup;
-		this.start;
-		this.placing;
-	}
+    static main() {
+        this.setup;
+        this.start;
+        this.placing;
+    }
 
-	static setup() {
-		const playerName = document.querySelector('.playerTitle');
-		this.player = new Player(playerName.textContent);
-		this.cpu = new Player('CPU');
-		this.dir = this.player.toPlace[0].dir;
+    static setup() {
+        const playerName = document.querySelector('.playerTitle');
+        this.player = new Player(playerName.textContent);
+        this.cpu = new Player('CPU');
+        this.dir = this.player.toPlace[0].dir;
 
-		this.player.setOpponent(cpu);
-		this.cpu.setOpponent(player);
-	}
+        this.player.setOpponent(cpu);
+        this.cpu.setOpponent(player);
+    }
 
-	static async placing() {
-		while (this.player.toPlace.length > 0) {
-			UI.availables(this.player.toPlace[0]);
-			let x = await UI.getUserX();
-			let y = await UI.getUserY();
-			this.player.gameboard.placeShip(x, y, toPlace[0].dir, toPlace[0]);
-		}
-	}
+    static async placing() {
+        while (this.player.toPlace.length > 0) {
+            UI.availables(this.player.toPlace[0]);
+            const clicked = await this.searchForClicked;
+            let x = clicked.getAttribute('data-x');
+            let y = clicked.getAttribute('data-y');
+            this.player.gameboard.placeShip(x, y, toPlace[0].dir, toPlace[0]);
+        }
+    }
 
-	static start() {
-		UI.startGame;
-	}
+    static searchForClicked() {
+        let clicked = undefined;
 
-	static end() {
-		UI.endGameModal;
-	}
+        while (clicked == undefined) {
+            clicked = document.querySelector('.clicked');
+        }
+
+        return clicked;
+    }
+
+    static switchDirection() {}
+
+    static start() {
+        UI.startGame;
+    }
+
+    static end() {
+        UI.endGameModal;
+    }
 }
